ba289f4d4ac360efb0e0421d2f5c4715f997c002
amankalmodiya <amankalmodiya9981@gmail.com>
2026-02-03 18:29:12 +0530

    Number Contains Specific Digit

---
+++ numberContainDigit.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using  namespace std;
+
+int numberContainDigit(int number,int specificDigit){
+    int digit=0;
+
+    while(number!=0){
+        digit=number%10;
+        number=number/10;
+
+        if(digit==specificDigit){
+            cout<<"Yes number contain this digit:";
+            return digit;
+        }
+
+    }
+
+      if(digit==specificDigit){
+            cout<<"Yes number contain this digit:";
+            return digit;
+        }
+        else{
+            cout<<"Number `not` contain this digit:";
+        }
+
+    return 0;
+}
+
+int main(){
+    int number,digit;
+
+    cout<<"Enter number:";
+    cin>>number;
+    cout<<"Enter Digit:";
+    cin>>digit;
+
+    numberContainDigit(number,digit);
+
+    return 0;
+}
